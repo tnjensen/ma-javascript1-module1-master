635bc287a5eef61bab60268488933c5b2ee3ba66
tnjensen <thomasnorupjensen@gmail.com>
2021-10-31 10:42:54 +0100

    Added answers for all questions

--- js/script.js
+++ js/script.js
@@ -12,3 +12,57 @@ const cats = [
     }
 ];
 
+/* Question 1 */
+const cat = {
+    complain: function(){
+        console.log('Meow!');  
+    }   
+}
+
+/* Question 2 */
+const heading = document.querySelector('h3');
+heading.innerHTML = "Updated heading";
+
+/* Question 3 */
+heading.style.fontSize = "2em";
+
+/* Question 4 */
+heading.classList.add("subheading");
+
+/* Question 5 */
+const paragraphs = document.querySelectorAll('p');
+for(let i = 0; i < paragraphs.length; i++){
+    paragraphs[i].style.color = "red";
+}
+
+/* Question 6 */
+const resultsContainer = document.querySelector('.results');
+resultsContainer.innerHTML = `<p style="background-color: yellow">New paragraph</p>`;
+
+/* Question 7 */
+function catsList(list){
+    for(let i = 0; i < list.length; i++){
+        console.log(list[i].name);
+    }
+}
+catsList(cats);
+
+/* Question 8 */
+function createCats(cats){
+    let html = "";
+    for(let i = 0; i < cats.length; i++){
+        let age = "Age unknown";
+        if(cats[i].age){
+            age = cats[i].age;
+        }
+        html += `<div>
+            <h5>Name: ${cats[i].name}</h5>
+            <p>Age: ${age}</p>
+        </div>`;
+    }
+    return html;
+}
+const html = createCats(cats);
+const catContainer = document.querySelector('.cat-container');
+catContainer.innerHTML = html;
+
